a6417a279d5cb1d6e87dc724ccc8fd3d3e355831
Rina-ui <marinagracia895@gmail.com>
2026-01-03 18:53:33 +0000

    mise en place du user repository

---
+++ src/main/java/glsi/api/tp_jee_egab/Repository/UserRepository.java
@@ -0,0 +1,13 @@
+package glsi.api.tp_jee_egab.Repository;
+
+import glsi.api.tp_jee_egab.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+
+}
