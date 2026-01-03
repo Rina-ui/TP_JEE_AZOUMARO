b1ba6d79ebcba19c9e44854d0cc38f3a7ae99887
Rina-ui <marinagracia895@gmail.com>
2026-01-03 17:12:18 +0000

    first commit

---
+++ src/main/java/glsi/api/tp_jee_egab/TpJeeEgabApplication.java
@@ -0,0 +1,13 @@
+package glsi.api.tp_jee_egab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpJeeEgabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpJeeEgabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/glsi/api/tp_jee_egab/TpJeeEgabApplicationTests.java
@@ -0,0 +1,13 @@
+package glsi.api.tp_jee_egab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpJeeEgabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
