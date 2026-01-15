b3be4973fef2463eb66b7b8ce5778a9c36eae1f4
Rina-ui <marinagracia895@gmail.com>
2026-01-15 09:16:43 +0000

    finish transaction and graphql

--- src/main/java/glsi/api/tp_jee_egab/Resolver/TransactionResolver.java
+++ src/main/java/glsi/api/tp_jee_egab/Resolver/TransactionResolver.java
@@ -3,10 +3,16 @@ package glsi.api.tp_jee_egab.Resolver;
 import glsi.api.tp_jee_egab.DTO.TransactionDTO;
 import glsi.api.tp_jee_egab.Service.TransactionService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -15,8 +21,54 @@ class TransactionResolver {
     private final TransactionService transactionService;
 
     @MutationMapping
-    TransactionDTO makeTransaction(@RequestBody TransactionDTO transactionDTO) {
-        return transactionDTO;
+    public TransactionDTO versement(@Argument VersementInput input) {
+        return transactionService.versement(
+                input.numeroCompte(),
+                BigDecimal.valueOf(input.montant()),
+                input.description()
+        );
     }
+
+    @MutationMapping
+    public TransactionDTO retrait(@Argument RetraitInput input) {
+        return transactionService.retrait(
+                input.numeroCompte(),
+                BigDecimal.valueOf(input.montant()),
+                input.description()
+        );
+    }
+
+    @MutationMapping
+    public TransactionDTO virement(@Argument VirementInput input) {
+        return transactionService.virement(
+                input.numeroCompteSource(),
+                input.numeroCompteDestination(),
+                BigDecimal.valueOf(input.montant()),
+                input.description()
+        );
+    }
+
+    @QueryMapping
+    public List<TransactionDTO> getAllTransactions(@Argument String numeroCompte) {
+        return transactionService.getAllTransactions(numeroCompte);
+    }
+
+    @QueryMapping
+    public List<TransactionDTO> getTransactionsByPeriode(
+            @Argument String numeroCompte,
+            @Argument String dateDebut,
+            @Argument String dateFin) {
+
+        LocalDateTime debut = LocalDateTime.parse(dateDebut);
+        LocalDateTime fin = LocalDateTime.parse(dateFin);
+
+        return transactionService.getTransactionsByPeriode(numeroCompte, debut, fin);
+    }
+
+    // Records pour les inputs
+    record VersementInput(String numeroCompte, double montant, String description) {}
+    record RetraitInput(String numeroCompte, double montant, String description) {}
+    record VirementInput(String numeroCompteSource, String numeroCompteDestination,
+                         double montant, String description) {}
 
 }
